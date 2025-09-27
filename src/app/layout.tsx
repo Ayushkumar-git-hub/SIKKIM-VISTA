c6e7c919d7c353d5fc458a2671584ac697b5eee4
Ayush Kumar <ayushbeastboy@gmail.com>
2025-09-27 09:17:37 +0000

    improve lighthouse performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,21 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
+import { PT_Sans, Playfair_Display } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-pt-sans',
+});
+
+const playfairDisplay = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-playfair-display',
+});
+
 
 export const metadata: Metadata = {
   title: 'Sikkim Explorer',
@@ -15,21 +30,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-            <Toaster />
-        </ThemeProvider>
+      <body className={cn("font-body antialiased", ptSans.variable, playfairDisplay.variable)}>
+        <React.StrictMode>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+          >
+              {children}
+              <Toaster />
+          </ThemeProvider>
+        </React.StrictMode>
       </body>
     </html>
   );
